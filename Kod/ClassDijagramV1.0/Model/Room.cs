265b2ea95f7d0a5df827514f514e65a7ff5809be
milana dokic <milana.dokic.md@gmail.com>
2022-04-10 01:05:19 +0200

    CRUD za sale

--- Kod/ClassDijagramV1.0/Model/Room.cs
+++ Kod/ClassDijagramV1.0/Model/Room.cs
@@ -8,7 +8,24 @@ using System;
 
 namespace Model
 {
-   public class Room
-   {
-   }
+    public class Room
+    {
+        public Room()
+        {
+
+        }
+        public Room(String RoomID, RoomName RoomName, int Floor, int RoomNumber, RoomStatus RoomStatus)
+        {
+            this.RoomID = RoomID;
+            this.RoomName = RoomName;
+            this.Floor = Floor;
+            this.RoomNumber = RoomNumber;
+            this.RoomStatus = RoomStatus;
+        }
+        public String RoomID { get; set; }
+        public RoomName RoomName { get; set; }
+        public int Floor { get; set; }
+        public int RoomNumber { get; set; }
+        public RoomStatus RoomStatus { get; set; }
+    }
 }
