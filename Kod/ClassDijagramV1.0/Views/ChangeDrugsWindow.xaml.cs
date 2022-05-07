f664686b4c8308880ef72ca23a732b46b074d45e
milana dokic <milana.dokic.md@gmail.com>
2022-05-07 13:38:53 +0200

    Dodata arhitektura i windows

---
+++ Kod/ClassDijagramV1.0/Views/ChangeDrugsWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClassDijagramV1._0.Views
+{
+    /// <summary>
+    /// Interaction logic for ChangeDrugsWindow.xaml
+    /// </summary>
+    public partial class ChangeDrugsWindow : Window
+    {
+        public ChangeDrugsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveChangedDrugs_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void QuitChangedDrugs_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
