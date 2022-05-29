d93e0547c0d7205a7fe37d4ed80d18aa2be639e1
Đorđe <the2922000@gmail.com>
2022-05-29 11:51:40 +0200

    Dodat izvjestaj od doktora

---
+++ Kod/ClassDijagramV1.0/Model/MedicalReport.cs
@@ -0,0 +1,79 @@
+﻿using ClassDijagramV1._0.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDijagramV1._0.Model
+{
+    public class MedicalReport : ObservableObject
+    {
+        public static int idCounter = 0;
+        private int id;
+        private string description;
+        private List<String> medicine;
+        private string note;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (description != value)
+                {
+                    description = value;
+                    OnPropertyChanged("Description");
+                }
+            }
+        }
+
+        public List<String> Medicine
+        {
+            get { return medicine; }
+            set
+            {
+                if (medicine != value)
+                {
+                    medicine = value;
+                    OnPropertyChanged("Therapy");
+                }
+            }
+        }
+
+        public String Note
+        {
+            get
+            {
+                return note;
+            }
+            set
+            {
+                note = value;
+                OnPropertyChanged("Note");
+            }
+        }
+
+        public MedicalReport(string description, List<String> medicine, string note)
+        {
+            Id = ++idCounter;
+            Description = description;
+            Medicine = medicine;
+            Note = note;
+        }
+    }
+}
